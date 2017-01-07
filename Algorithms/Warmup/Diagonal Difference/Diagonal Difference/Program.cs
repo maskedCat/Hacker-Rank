3154d3ef9ccc0a6418fca4d3c112385e8b91dfe8
Michael <michael.cullen81@gmail.com>
2017-01-07 15:03:49 +0000

    First commit

---
+++ Algorithms/Warmup/Diagonal Difference/Diagonal Difference/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var matrix = new int[n, n];
+                       
+            do
+            {
+                var 
+                n--;
+            } while (n > 0);
+
+        }
+
+        private static int[] ConvertToArray(string input)
+        {
+            return Array.ConvertAll(input.Split(' '), Int32.Parse);
+        }
+    }
+}
