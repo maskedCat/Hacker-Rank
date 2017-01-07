b4786ae985d6bc000712c56cd148c01d6836f733
Michael <michael.cullen81@gmail.com>
2017-01-07 14:43:16 +0000

    Solved

---
+++ Algorithms/Warmup/A Very Big Sum/A Very Big Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace A_Very_Big_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Console.ReadLine();
+            var a = ConvertToArray(Console.ReadLine());
+            long result = 0;
+
+            foreach(var i in a)
+            {
+                result += i;
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static int[] ConvertToArray(string input)
+        {
+            return Array.ConvertAll(input.Split(' '), Int32.Parse);
+        }
+    }
+}
