b95b1f55abed3872b15d02c437e3b0bcb3641d98
Michael <michael.cullen81@gmail.com>
2017-01-07 14:03:45 +0000

    First commit

---
+++ Algorithms/Warmup/Solve me first/Solve me first/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solve_me_first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
