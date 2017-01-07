5a91358c090f80c2523b1b7daa7176213dbd529b
Michael <michael.cullen81@gmail.com>
2017-01-07 14:17:45 +0000

    Solved

---
+++ Algorithms/Warmup/Simple Array Sum/Simple Array Sum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Simple_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var arrayRaw = Console.ReadLine().Split(' ');
+            var array = Array.ConvertAll<string, int>(arrayRaw, Int32.Parse);
+            var sum = array.Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
