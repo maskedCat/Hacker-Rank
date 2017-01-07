fd7318f31485fa0ec2b4ab2968788fcf5bc6fbd8
Michael <michael.cullen81@gmail.com>
2017-01-07 14:37:31 +0000

    Solved

---
+++ Algorithms/Warmup/Compare the Triplets/Compare the Triplets/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Compare_the_Triplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = ConvertToArray(Console.ReadLine());
+            var b = ConvertToArray(Console.ReadLine());
+
+            var scores = CalculateScores(a, b);
+            Console.WriteLine(string.Join(" ", scores.ScoreA, scores.ScoreB));
+        }
+
+        private static int[] ConvertToArray(string input)
+        {
+            return  Array.ConvertAll(input.Split(' '), Int32.Parse);
+        }
+
+        private static Result CalculateScores(int[] a, int[]b)
+        {
+            var result = new Result();
+
+            for(var i =0; i< a.Length; i++)
+            {
+                var elementA = a[i];
+                var elementB = b[i];
+
+                if(elementA > elementB)
+                {
+                    result.ScoreA++;
+                }
+                else if(elementB > elementA)
+                {
+                    result.ScoreB++;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    struct Result
+    {
+        public int ScoreA { get; set; }
+        public int ScoreB { get; set; }
+    }
+}
