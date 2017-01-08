1fe337ff0720ab3032768ce3b48a7f381a6939e3
Michael <michael.cullen81@gmail.com>
2017-01-08 15:58:47 +0000

    Solved

--- Algorithms/Warmup/Diagonal Difference/Diagonal Difference/Program.cs
+++ Algorithms/Warmup/Diagonal Difference/Diagonal Difference/Program.cs
@@ -12,18 +12,61 @@ namespace Diagonal_Difference
         {
             var n = Convert.ToInt32(Console.ReadLine());
             var matrix = new int[n, n];
-                       
-            do
-            {
-                var 
-                n--;
-            } while (n > 0);
 
+            for (var i = 0; i < n; i++)
+            {
+                var currentLine = ConvertToArray(Console.ReadLine());
+                for (var j = 0; j < currentLine.Length; j++)
+                {
+                    matrix[i, j] = currentLine[j];
+                }
+            }
+
+            var primaryDiagonal = CalculatePrimaryDiagonal(matrix, n);
+            var secondaryDiagonal = CalculateSecondaryDiagonal(matrix, n);
+
+            var difference = primaryDiagonal - secondaryDiagonal;
+
+            Console.WriteLine(Math.Abs(difference));
         }
 
         private static int[] ConvertToArray(string input)
         {
             return Array.ConvertAll(input.Split(' '), Int32.Parse);
         }
+
+        private static int CalculatePrimaryDiagonal(int[,] array, int size)
+        {
+            var total = 0;
+            var currentRow = 0;
+            var currentColumn = 0;
+
+            do
+            {
+                total += array[currentRow, currentColumn];
+                currentRow++;
+                currentColumn++;
+            } while (currentRow < size);
+
+            return total;
+        }
+
+        private static int CalculateSecondaryDiagonal(int[,] array, int size)
+        {
+            var total = 0;
+            var currentRow = 0;
+            var currentColumn = size-1;
+
+            do
+            {
+                total += array[currentRow, currentColumn];
+                currentRow++;
+                currentColumn--;
+            } while (currentColumn >= 0);
+
+            return total;
+        }
+
+       
     }
 }
