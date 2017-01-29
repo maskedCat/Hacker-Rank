7a4b951727207a669c795a9a9d203f21f47964f3
Michael <michael.cullen81@gmail.com>
2017-01-29 11:30:25 +0000

    Completed

---
+++ Algorithms/Warmup/Staircase/Staircase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string currentSteps = string.Empty;
+            
+            for(int i = n; i > 0; i--)
+            {
+                currentSteps += "#";
+                Console.WriteLine(IndentLeft(i - 1) + currentSteps);
+            }
+        }
+
+        static string IndentLeft(int n)
+        {
+            return string.Empty.PadLeft(n);
+        }
+    }
+}
