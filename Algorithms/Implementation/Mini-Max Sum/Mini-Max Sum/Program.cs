588ed6bd8dac806b455863ba5847dbaaaa124ff0
Michael <michael.cullen81@gmail.com>
2017-01-29 18:35:58 +0000

    Solved

---
+++ Algorithms/Implementation/Mini-Max Sum/Mini-Max Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Mini_Max_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Array.ConvertAll<string,Int64>(Console.ReadLine().Split(' '),Int64.Parse);
+
+            var orderedInput = input.OrderBy(i => i).ToArray() ;
+
+            var maxSum = orderedInput.Skip(1).Sum();
+            var minSum = orderedInput.Take(4).Sum();
+
+            Console.WriteLine(minSum + " " + maxSum);            
+        }
+    }
+}
