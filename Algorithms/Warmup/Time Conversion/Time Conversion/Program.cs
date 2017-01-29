b4c23cf65f76ca9ad84b645ea9526fcf833b4892
Michael <michael.cullen81@gmail.com>
2017-01-29 12:07:12 +0000

    Solved

---
+++ Algorithms/Warmup/Time Conversion/Time Conversion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Time_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time = Console.ReadLine();
+            const string inputFormat = "hh:mm:sstt";
+            const string outputFormat = "HH:mm:ss";
+            var dateTime = DateTime.ParseExact(time, inputFormat, CultureInfo.CurrentCulture);
+            Console.WriteLine(dateTime.ToString(outputFormat));
+        }
+    }
+}
