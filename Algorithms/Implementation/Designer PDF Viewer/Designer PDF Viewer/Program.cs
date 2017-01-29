61721a38cdae5e5e817a7d82acb331a3a8dce8e2
Michael <michael.cullen81@gmail.com>
2017-01-29 19:00:57 +0000

    Solved

---
+++ Algorithms/Implementation/Designer PDF Viewer/Designer PDF Viewer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Designer_PDF_Viewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int width = 1;
+            
+            var heights = Array.ConvertAll(Console.ReadLine().Split(' '),int.Parse);
+            var word = Console.ReadLine();
+
+            var currentMaxHeight = 0;
+            var numberOfCharacters = 0;
+            foreach(var character in word)
+            {
+                var heightIndex = (int)character - 97;
+                var height = heights[heightIndex];
+                if (height > currentMaxHeight)
+                    currentMaxHeight = height;
+                numberOfCharacters++;
+            }
+
+            var area = numberOfCharacters * width * currentMaxHeight;
+            Console.WriteLine(area);
+        }
+    }
+}
